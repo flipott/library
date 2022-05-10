27d429a8eb6d7e087251f0f1ef18d0eea2c26c01
flipott <ottphillipa@gmail.com>
2022-05-10 12:54:24 -0400

    Created initial library object with add function

--- script.js
+++ script.js
@@ -1,50 +1,80 @@
 const addBtn = document.getElementById("add-btn");
-const removeBtn = document.getElementById("remove")
 const bookForm = document.getElementById("form-container");
 const cancelBtn = document.getElementById("cancel");
 const submitBtn = document.getElementById("submit");
-const table = document.getElementById("book-table")
+const table = document.getElementById("book-table");
+const form = document.getElementById("add-form");
 
 
 cancelBtn.addEventListener("click", hideForm);
 addBtn.addEventListener("click", showForm);
 submitBtn.addEventListener("click", addBook);
+document.querySelector('body').addEventListener('click', removeBook);
 
 function showForm() {
     bookForm.style.display = "flex";
 }
-
+ 
 function hideForm() {
     bookForm.style.display = "none";
 }
 
+let myLibrary = []
+
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
 function addBook() {
+
+    const author = form.elements['author'].value;
+    const title = form.elements['title'].value;
+    const pages = form.elements['pages'].value;
+    const readStatus = form.elements['status'].value;
+
     const newRow = document.createElement("tr");
 
     const newAuthor = document.createElement("td");
-    newAuthor.innerHTML = "JK Rowling"
+    newAuthor.innerHTML = author;
     newRow.appendChild(newAuthor);
 
     const newTitle = document.createElement("td");
-    newTitle.innerHTML = "Harry Potter 2"
+    newTitle.innerHTML = title;
     newRow.appendChild(newTitle);
 
     const newPages = document.createElement("td");
-    newPages.innerHTML = "555"
+    newPages.innerHTML = pages;
     newRow.appendChild(newPages);
 
     const newRead = document.createElement("td");
-    newRead.innerHTML = "No"
+    newRead.innerHTML = readStatus;
     newRow.appendChild(newRead);
 
     const newRemove = document.createElement("td");
-    newRemove.innerHTML = "<button>x</button>"
+    newRemove.innerHTML = `<button class="remove">x</button>`
     newRow.appendChild(newRemove);
 
+    const addToLibrary = new Book(author, title, pages, readStatus);
+    index = myLibrary.length;
+    console.log(index);
+    myLibrary.push(addToLibrary);
+    newRow.setAttribute("data-index", index);
     table.appendChild(newRow);
-
-    hideForm();    
+    hideForm();
+    form.reset();
 }
 
-
-// bookForm.style.display = "flex";
+function removeBook(e) {
+    if (e.target.className == "remove") {
+        let tempIndex = e.target.parentElement.parentElement.getAttribute("data-index");
+        console.log(tempIndex);
+        myLibrary.splice(tempIndex, 1);
+        e.target.parentElement.parentElement.remove();
+        console.log(myLibrary);
+    } else {
+        return;
+    }
+}
