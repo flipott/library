4b2d38dc4a49f72c646eb0be5646bfaece1d4b93
flipott <ottphillipa@gmail.com>
2022-05-21 21:41:30 -0400

    Finished table object behavior

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
+//Initialize variables
 const addBtn = document.getElementById("add-btn");
 const bookForm = document.getElementById("form-container");
 const cancelBtn = document.getElementById("cancel");
 const submitBtn = document.getElementById("submit");
-const table = document.getElementById("book-table");
+const table = document.getElementById("table-body");
 const form = document.getElementById("add-form");
-let newStatus;
 
-
+//Listen to buttons
 cancelBtn.addEventListener("click", hideForm);
 addBtn.addEventListener("click", showForm);
 submitBtn.addEventListener("click", validateForm);
 document.querySelector('body').addEventListener('click', editEntry);
 
+//Shows form
 function showForm() {
     bookForm.style.display = "flex";
 }
- 
+
+//Hides and resets form
 function hideForm() {
     bookForm.style.display = "none";
     form.reset();
 }
 
+//Array that stores book objects
 let myLibrary = []
 
+//Constructs a book object
 function Book(author, title, pages, read) {
     this.author = author;
     this.title = title;
@@ -30,67 +34,87 @@ function Book(author, title, pages, read) {
     this.read = read;
 }
 
-function addBook() {
+testBook = new Book("John Doe", "Traveling!", 500, "Not Read");
+testBookTwo = new Book("Joey Gafmen", "Spellitout", 222, "Read");
+myLibrary.push(testBook);
+myLibrary.push(testBookTwo);
+displayBooks();
 
+//Loops through myLibrary array and displays books accordingly
+function displayBooks() {  
+    let clearTable = document.getElementById('book-table').getElementsByTagName('tbody')[0]; 
+    clearTable.innerHTML = '';
+
+    for (let i=0; i<myLibrary.length; i++) {
+
+        const author = myLibrary[i].author;
+        const title = myLibrary[i].title;   
+        const pages = myLibrary[i].pages;
+        const readStatus = myLibrary[i].read;
+
+        const newRow = document.createElement("tr");
+
+        const newAuthor = document.createElement("td");
+        newAuthor.innerHTML = author;
+        newRow.appendChild(newAuthor);
+    
+        const newTitle = document.createElement("td");
+        newTitle.innerHTML = title;
+        newRow.appendChild(newTitle);
+    
+        const newPages = document.createElement("td");
+        newPages.innerHTML = pages;
+        newRow.appendChild(newPages);
+    
+        const newRead = document.createElement("td");
+        newRead.innerHTML = readStatus + `<button class="change-read">Change</button>`
+        newRow.appendChild(newRead);
+    
+        const newRemove = document.createElement("td");
+        newRemove.innerHTML = `<button class="remove"></button>`;
+        newRow.appendChild(newRemove);
+        
+        newRow.setAttribute("data-index", i);
+
+        table.appendChild(newRow);
+    }
+}
+
+//Constructs a book and adds it to the library
+function addBook() {
     const author = form.elements['author'].value;
     const title = form.elements['title'].value;
     const pages = form.elements['pages'].value;
     const readStatus = form.elements['status'].value;
-
-    const newRow = document.createElement("tr");
-
-    const newAuthor = document.createElement("td");
-    newAuthor.innerHTML = author;
-    newRow.appendChild(newAuthor);
-
-    const newTitle = document.createElement("td");
-    newTitle.innerHTML = title;
-    newRow.appendChild(newTitle);
-
-    const newPages = document.createElement("td");
-    newPages.innerHTML = pages;
-    newRow.appendChild(newPages);
-
-    const newRead = document.createElement("td");
-    newRead.innerHTML = readStatus + `<button class="change-read">Change</button>`
-    newRow.appendChild(newRead);
-
-    const newRemove = document.createElement("td");
-    newRemove.innerHTML = `<button class="remove"></button>`
-    newRow.appendChild(newRemove);
-
     const addToLibrary = new Book(author, title, pages, readStatus);
-    index = myLibrary.length;
-    console.log(index);
+
     myLibrary.push(addToLibrary);
-    newRow.setAttribute("data-index", index);
-    table.appendChild(newRow);
     hideForm();
     form.reset();
+    displayBooks();
 }
 
+//Edits book object according to button click
 function editEntry(e) {
+    let targetIndex = e.target.parentElement.parentElement.getAttribute("data-index");
+
     if (e.target.className == "remove") {
-        let tempIndex = e.target.parentElement.parentElement.getAttribute("data-index");
-        console.log(tempIndex);
-        myLibrary.splice(tempIndex, 1);
-        e.target.parentElement.parentElement.remove();
-        console.log(myLibrary);
+        myLibrary.splice(targetIndex, 1);
+        displayBooks()
     } else if (e.target.className == "change-read") {
          if (e.target.previousSibling.textContent == "Not Read") {
-            e.target.previousSibling.remove();
-            newStatus = document.createTextNode("Read")
-            e.target.parentElement.insertBefore(newStatus, e.target.parentElement.firstChild);
+            myLibrary[targetIndex].read = "Read";
+            displayBooks();
          } else {
-            e.target.previousSibling.remove();
-            newStatus = document.createTextNode("Not Read")
-            e.target.parentElement.insertBefore(newStatus, e.target.parentElement.firstChild);
+            myLibrary[targetIndex].read = "Not Read";
+            displayBooks();
          }
     } else {
         return;
     }
 }
 
+//Makes sure correct form info is submitted
 function validateForm() {
     const author = form.elements['author'].value;
     const title = form.elements['title'].value;
@@ -112,5 +136,4 @@ function validateForm() {
     else {
         return addBook();
     }
-
 }
